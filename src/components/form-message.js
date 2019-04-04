2654ddad1982bf0273eec5d3fc5816585594dd67
Alan Reis Belarmino <alanreisb@gmail.com>
2019-04-04 14:27:16 -0300

    Contato Mapa

--- src/components/form-message.js
+++ src/components/form-message.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
-const formClasse = 'col-6 h-100 float-right position-relative   rounded p-3 pl-5';
+const formClasse = 'col-6 h-100 float-right position-relative   rounded p-4 pl-5';
 
 const formCss = {
     backgroundColor:'black',
      height:'100%',
      fontFamily:'arial !important', 
-     marginRight:'6%',
+     marginRight:'10%',
      background:'rgba(50,50,50,.3)',
+}
+
+const tituloCss = {
+    fontFamily: 'arial !important',
+    color:'white',
+    fontWeight:'600',
+    
 }
 
 const inputClass = 'col-11 p-2 rounded';
@@ -27,7 +34,6 @@ const botaoClasse = 'btn btn-dark float-right mt-2 btn-lg col-4';
 
 const botaoCss = {
     marginRight:'8%',
-    
     color:'white',
     fontWeight:'600',
     fontSize:'1.2em'
@@ -38,19 +44,17 @@ class FormMessage extends Component {
         return (
             <div>
                 <form className={formClasse} style={formCss}>
-                    
+                    <h3 style={tituloCss}>Mensagem:</h3>
+
                     <label className={labelClass} style={labelCss}>Nome</label>
-                    
                     <input type='text'  className={inputClass} style={inputCss}></input>
                     
                     
                     
                     <label  className={labelClass} style={labelCss}>Email</label>
-                    
                     <input type='email' className={inputClass}></input>
                     
                     <label  className={labelClass} style={labelCss}>Mensagem</label>
-                    
                     <textarea className='col-11 p-2 rounded' rows='5' style={{resize:'none'}}></textarea>
 
                     <input type='submit'className={botaoClasse} style={botaoCss}></input>
--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -5,6 +5,9 @@ import FormMessage from '../components/form-message';
 import fbIcon from '../components/imagens/icones contato/facebook.png';
 import wpIcon from '../components/imagens/icones contato/whatsapp.png';
 import lkIcon from '../components/imagens/icones contato/linkedin.png';
+
+
+
 const tituloCss = {
     fontFamily: 'Azonix'
 }
@@ -12,14 +15,12 @@ const tituloCss = {
 const textoCss = {
     float: 'left',
     color: 'white',
-    backgroundColor: 'black',
+    background:'rgba(50,50,50,.3)',
     left: '10%',
-    top: '40%',
     position: 'relative',
-    transform: 'translateY(-50%)',
-    height: '80%',
     width: '35%',
     fontFamily: 'arial !important'
+    
 
 };
 const contatosLiClasse = 'p-2 ';
@@ -37,16 +38,18 @@ class Contato extends Component {
 
                 <MenuLateral></MenuLateral>
 
-                <div style={textoCss}>
-                    <h1 className='display-5 mb-4'>Entre em contato...</h1>
-                    <p className='text-justify' style={{ fontFamily: 'arial', lineHeight: '2', fontWeight: 'bold', marginTop: '20%' }}>Cidade: Cruzeiro-SP<br />
+                <div style={textoCss} className='rounded pl-4 pr-4 pt-4 pb-4'>
+                    <h1 className='display-5 mb-2'>Entre em contato...</h1>
+                    <p className='text-justify m-0' style={{ fontFamily: 'arial', lineHeight: '2', fontWeight: 'bold' }}>Cidade: Cruzeiro-SP<br />
                         CEP: 12.711-450<br />
                         Tel: (12) 98250-1585</p>
-                    <ul className='flex-row d-flex text-light mt-1'>
+                    <ul className='flex-row d-flex text-light m-0 mb-2 p-0'>
                         <li className={contatosLiClasse}><img src={fbIcon} style={iconContatoCss}></img></li>
                         <li className={contatosLiClasse}><img src={lkIcon} style={iconContatoCss}></img></li>
                         <li className={contatosLiClasse}><img src={wpIcon} style={iconContatoCss}></img></li>
                     </ul>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58946.8210909044!2d-44.99800465803306!3d-22.5725079066739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9df67fe73253c7%3A0x17d1c1765e1b1017!2sCruzeiro%2C+SP!5e0!3m2!1spt-BR!2sbr!4v1554397277004!5m2!1spt-BR!2sbr"  classname='rounded' width="100%" height="250" frameborder="0"  allowfullscreen></iframe>
+
                 </div>
 
                 <FormMessage></FormMessage>
